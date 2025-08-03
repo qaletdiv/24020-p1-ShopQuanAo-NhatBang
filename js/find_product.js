7aca306b11d66dff15dde41a978e5a82c5bb127e
Bang <nnb24.112003@gmail.com>
2025-08-03 15:50:20 +0700

    hoan thanh web shopping

--- js/find_product.js
+++ js/find_product.js
@@ -84,13 +84,13 @@ if (searchKey) {
 
       divProductHtml.innerHTML = `
         <div class="img_hidden">
-          <a href="" class="img_box">
+          <a href="product-detail.html?id=${item.id}" class="img_box">
             <img src="${item.imageURL}" alt="${item.name}" />
             <div class="product_overlay"></div>
             ${saleHTML}
           </a>
         </div>
-        <a href="" class="product_name">${item.name}</a>
+        <a href="product-detail.html?id=${item.id}" class="product_name">${item.name}</a>
         <div class="money_sale">
           ${priceHTML}
         </div>
